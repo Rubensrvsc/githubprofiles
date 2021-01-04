ce8b2a2b3ec9acc0100f0e4ed7c96091399fd429
rubensrvsc <Rubensspfc100@gmail.com>
2021-01-03 21:39:44 -0300

    refactor(IndexView): mudando a classe herdade de TemplateView para View

--- appprofiles/views.py
+++ appprofiles/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views import View
 
 # Create your views here.
 
-class IndexView(TemplateView):
+class IndexView(View):
     template_name = "index.html"
+
+    def get(self, request):
+        return render(request, self.template_name)
