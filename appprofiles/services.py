b04044dbc57bd6103d472bf9b02856bbebc6291e
rubensrvsc <Rubensspfc100@gmail.com>
2021-01-07 08:54:46 -0300

    refactor(services.py): mostrando os ultimos repositorios na funcao repository

--- appprofiles/services.py
+++ appprofiles/services.py
@@ -1,8 +1,16 @@
 from .utils import *
 import requests
+import json
 
 def profiles(name):
     return requests.get(profile + str(name)).json()
 
 def repositories(name):
-    return requests.get(profile + str(name) + '/repos').json()
+    repos = requests.get(profile + str(name) + '/repos').json()
+    repository = []
+    count = 0
+    while count < 4:
+        repository.append(repos[count])
+        count = count + 1
+
+    return repository
